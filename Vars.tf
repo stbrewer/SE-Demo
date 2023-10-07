4af1d594ba67342613469303f19cf2afc1b4f703
Steve Brewer <brewer.stephen.c@gmail.com>
2023-10-06 20:40:52 -0500

    Update Vars.tf

--- Vars.tf
+++ Vars.tf
@@ -11,34 +11,34 @@ variable "AWS_Region" {
 
 variable "AWS_Transit_GW" {
   type        = string
-  default     = "7eleven-AWS-Transit-GW"
+  default     = "customername-AWS-Transit-GW"
 }
 
 variable "AWS_Transit_VPC" {
   type        = string
-  default     = "7eleven-AWS-Transit"
+  default     = "customername-AWS-Transit"
 }
 
 variable "AWS_Spoke_GW_1" {
   type        = string
-  default     = "7eleven-AWS-Spoke-GW-1"
+  default     = "customername-AWS-Spoke-GW-1"
 }
 
 variable "AWS_Spoke_VPC_1" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-AWS-VPC-Spoke-1"
+  default     = "customername-AWS-VPC-Spoke-1"
 }
 
 variable "AWS_Spoke_GW_2" {
   type        = string
-  default     = "7eleven-AWS-Spoke-GW-2"
+  default     = "customername-AWS-Spoke-GW-2"
 }
 
 variable "AWS_Spoke_VPC_2" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-AWS-VPC-Spoke-2"
+  default     = "customername-AWS-VPC-Spoke-2"
 }
 
 variable "Azure_Region" {
@@ -52,97 +52,97 @@ variable "Azure_Resource_Group" {
 variable "Azure_Spoke_VPC_1" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-Azure-VPC-Spoke-1"
+  default     = "customername-Azure-VPC-Spoke-1"
 }
 
 variable "Azure_Spoke_GW_1" {
   type        = string
-  default     = "7eleven-Azure-Spoke-GW-1"
+  default     = "customername-Azure-Spoke-GW-1"
 }
 
 variable "Azure_Spoke_VPC_2" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-Azure-VPC-Spoke-2"
+  default     = "customername-Azure-VPC-Spoke-2"
 }
 
 variable "Azure_Spoke_GW_2" {
   type        = string
-  default     = "7eleven-Azure-Spoke-GW-2"
+  default     = "customername-Azure-Spoke-GW-2"
 }
 
 variable "Azure_Transit_VPC" {
   type        = string
-  default     = "7eleven-Azure-Transit"
+  default     = "customername-Azure-Transit"
 }
 
 variable "Azure_Transit_GW" {
   type        = string
-  default     = "7eleven-Azure-Transit-GW"
+  default     = "customername-Azure-Transit-GW"
 }
 
 variable "GCP_Spoke_VPC_1" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-gcp-gcp-spoke-1"
+  default     = "customername-gcp-gcp-spoke-1"
 }
 
 variable "GCP_Spoke_GW_1" {
   type        = string
-  default     = "7eleven-gcp-spoke-gw-1"
+  default     = "customername-gcp-spoke-gw-1"
 }
 
 variable "GCP_Spoke_VPC_2" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-gcp-vpc-spoke-2"
+  default     = "customername-gcp-vpc-spoke-2"
 }
 
 variable "GCP_Spoke_GW_2" {
   type        = string
-  default     = "7eleven-gcp-spoke-gw-2"
+  default     = "customername-gcp-spoke-gw-2"
 }
 
 variable "GCP_Transit_VPC" {
   type        = string
-  default     = "7eleven-gcp-transit"
+  default     = "customername-gcp-transit"
 }
 
 variable "GCP_Transit_GW" {
   type        = string
-  default     = "7eleven-gcp-transit-gw"
+  default     = "customername-gcp-transit-gw"
 }
 
 variable "OCI_Spoke_VPC_1" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-OCI-OCI-Spoke-1"
+  default     = "customername-OCI-OCI-Spoke-1"
 }
 
 variable "OCI_Spoke_GW_1" {
   type        = string
-  default     = "7eleven-OCI-Spoke-GW-1"
+  default     = "customername-OCI-Spoke-GW-1"
 }
 
 variable "OCI_Spoke_VPC_2" {
   description = "The CIDR block for the VPC."
   type        = string
-  default     = "7eleven-OCI-VPC-Spoke-2"
+  default     = "customername-OCI-VPC-Spoke-2"
 }
 
 variable "OCI_Spoke_GW_2" {
   type        = string
-  default     = "7eleven-OCI-Spoke-GW-2"
+  default     = "customername-OCI-Spoke-GW-2"
 }
 
 variable "OCI_Transit_VPC" {
   type        = string
-  default     = "7eleven-OCI-Transit"
+  default     = "customername-OCI-Transit"
 }
 
 variable "OCI_Transit_GW" {
   type        = string
-  default     = "7eleven-OCI-Transit-GW"
+  default     = "customername-OCI-Transit-GW"
 }
 
 variable "Prod_Smartgroup" {
